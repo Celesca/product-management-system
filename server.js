02a1e63ec594a1577ab69ef35bee336ac3d1413c
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-26 02:43:50 +0700

    ✅ add: config server.js

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+// GET request
+app.get('/products', (req, res) => {
+  res.send('Hello World!');
+});
+
+// POST request
+app.post('/products', (req, res) => {
+    res.send('Add a new product');
+})
+
+// PUT request
+app.put('/products/:id', (req, res) => {
+    res.send('Update the product with id ' + req.params.id);
+})
+
+// DELETE request
+app.delete('/products/:id', (req, res) => {
+    res.send('Delete the product with id ' + req.params.id);
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server running at <http://localhost>:${port}/`);
 });
