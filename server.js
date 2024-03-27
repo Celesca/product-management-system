002b94d935a3aecae42c2523663af03f1433871c
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-28 01:07:55 +0700

    🔧  add: POST /products

--- server.js
+++ server.js
@@ -33,13 +33,45 @@ app.get('/products/:id', (req, res) => {
 
 // POST request
 app.post('/products', (req, res) => {
-    res.send('Add a new product');
+  if (!req.body.name || !req.body.category || !req.body.price || !req.body.stock) {
+    return res.status(400).send({message: 'All fields are required'});
+  }
+  if (req.body.price <= 0 || req.body.stock <= 0) {
+    return res.status(400).send({message: 'Price and Stock must be greater than 0'});
+  }
+    const newProduct = {
+      id: products.length + 1,
+      name: req.body.name,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock
+    };
+    products.push(newProduct);
+    res.json(newProduct);
 })
 
 // PUT request
 app.put('/products/:id', (req, res) => {
-    res.send('Update the product with id ' + req.params.id);
-})
+  const product = products.find(p => p.id === parseInt(req.params.id));
+  if (!product) {
+    return res.status(404).send({"message" : "Product not found"});
+  }
+
+  product.name = req.body.name;
+  product.category = req.body.category;
+  product.price = req.body.price;
+  product.stock = req.body.stock;
+
+  if (!req.body.name || !req.body.category || !req.body.price || !req.body.stock) {
+    return res.status(400).send({message: 'All fields are required'});
+  }
+
+  if (req.body.price <= 0 || req.body.stock <= 0) {
+    return res.status(400).send({message: 'Price and Stock must be greater than 0'});
+  }
+
+  res.json(product);
+});
 
 // DELETE request
 app.delete('/products/:id', (req, res) => {
--- tests/server.test.js
+++ tests/server.test.js
@@ -12,7 +12,7 @@ describe('GET /products', () => {
     })
 })
 
-describe('GET /products/:id', () => {
+describe('GET /products/:id normal', () => {
     it("should return a single product", async() => {
         const res = await request(app).get("/products/1");
         expect(res.statusCode).toBe(200);
@@ -33,5 +33,84 @@ describe('GET /products/:id with invalid id', () => {
         const res = await request(app).get("/products/-1");
         expect(res.statusCode).toBe(400);
         expect(res.body.message).toStrictEqual("Invalid ID");
+    })
+})
+
+describe('POST /products normal', () => {
+    it("should create a new product", async() => {
+        const res = await request(app).post("/products").send({
+            name: "New Product",
+            category: "New Category",
+            price: 100,
+            stock: 10
+        });
+        const addProductResponse = {
+            id: 3,
+            name: "New Product",
+            category: "New Category",
+            price: 100,
+            stock: 10
+        };
+        expect(res.statusCode).toBe(200);
+        expect(res.body.id).toBe(3);
+        expect(res.body).toStrictEqual(addProductResponse);
+    })
+})
+
+describe('POST /products Invalid Request', () => {
+    it("should return a 400", async() => {
+        const res = await request(app).post("/products").send({
+            name: "New Product",
+            category: "New Category",
+            price: 100
+        });
+        expect(res.statusCode).toBe(400);
+        expect(res.body.message).toStrictEqual("All fields are required");
+    })
+});
+
+describe('POST /products Minus stock', () => {
+    it("should return a 400", async() => {
+        const res = await request(app).post("/products").send({
+            name: "New Product",
+            category: "New Category",
+            price: 100,
+            stock: -10
+        });
+        expect(res.statusCode).toBe(400);
+        expect(res.body.message).toStrictEqual("Price and Stock must be greater than 0");
+    })
+});
+
+describe('PUT /products normal', () => {
+    it("should update a product", async() => {
+        const res = await request(app).put("/products/3").send({
+            name: "Updated Product",
+            category: "Updated Category",
+            price: 200,
+            stock: 20
+        });
+        const updatedProduct = {
+            id: 3,
+            name: "Updated Product",
+            category: "Updated Category",
+            price: 200,
+            stock: 20
+        };
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toStrictEqual(updatedProduct);
+    })
+});
+
+describe('PUT /products with no id in products', () => {
+    it("should return a 404", async() => {
+        const res = await request(app).put("/products/100").send({
+            name: "Updated Product",
+            category: "Updated Category",
+            price: 200,
+            stock: 20
+        });
+        expect(res.statusCode).toBe(404);
+        expect(res.body.message).toStrictEqual("Product not found");
     })
 })
